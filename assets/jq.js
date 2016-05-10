82278789c25ee0b6b1b0ec520d15cdb501154499
Yassi <yassih84@gmail.com>
2016-05-09 23:00:27 -0400

    responsive navbar

---
+++ assets/jq.js
@@ -0,0 +1,9 @@
+
+$( document ).ready(function() {
+    $(".toggle-nav").click(function(){
+    	$("#side-nav").toggleClass("left");
+    	// $("#side-nav").css("left","0px");
+    	// console.log( "ready!" );
+    })
+});
+
