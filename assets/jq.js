0f71887adf62f88e872a933daeb3382ec064ab32
Yassi <yassih84@gmail.com>
2016-05-17 23:04:39 -0400

    removing extra nav and seperating css files

--- assets/jq.js
+++ assets/jq.js
@@ -1,9 +1,7 @@
 
 $( document ).ready(function() {
     $(".toggle-nav").click(function(){
-    	$("#side-nav").toggleClass("left");
-    	// $("#side-nav").css("left","0px");
-    	// console.log( "ready!" );
+    	$("nav ul").toggleClass("show");
     })
 });
 
